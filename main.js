87bfda68d5230f7ac8df616484a105f40dc63190
Sahil Mistry <sahil.m@simformsolutions.com>
2023-04-18 16:27:49 +0530

    feat: add squre, squre-root, modulas, exp, x-pow-y, 10-pow-x, 1-divide-x functionallitly

--- main.js
+++ main.js
@@ -26,21 +26,43 @@ const getclickedButtonId = function(event) {
         }
     }
 }
-const reduceButtonSize = function(event) {
-    const clickedButton = document.getElementById(getclickedButtonId(event));
-    
-    clickedButton.style.border = '5px solid black';
-    clickedButton.style.boxSizing = 'border-box'
-}
 
-const operatorNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "/", "mod", "*", "(", ")"];
-let inputString = "";
-function handleClick(event) {
-
-    // reduceButtonSize(event);
+const getDisplay = function() {
     let input = document.querySelector(".question");
     let output = document.querySelector(".answer");
-    
+
+    return [input, output];
+}
+const showError = function(errMsg) {
+    let output = getDisplay()[1];
+    output.innerText = errMsg;
+    output.style.color = "red";
+}
+const validInput = function(inputMessage) {
+    if(Number(inputMessage) !== NaN) {
+        console.log(Number(inputMessage));
+        return true;
+    } 
+    return false;
+}
+
+const operatorNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "÷", "mod", "*", "(", ")", "π", "e"];
+const xTypefunction = ["**2", "**1/2", "||", "e**", "**-1", "**", "10**"];
+
+function validString(givenString) {
+    return  (givenString
+            .replaceAll("mod", "%")
+            .replaceAll("÷", "/")
+            .replaceAll("π", Math.PI)
+            .replaceAll("e", Math.E));
+}
+let inputString = "";
+
+function handleClick(event) {
+
+    let input = getDisplay()[0];
+    let output = getDisplay()[1];
+
     let clickedButtonId = getclickedButtonId(event); 
     switch (clickedButtonId) {
         case "AC":
@@ -50,35 +72,84 @@ function handleClick(event) {
             break;
 
         case "clear":
-            console.log(input.innerText + " " + clickedButtonId);
             input.innerText = input.innerText.slice(0, input.innerText.length - 1);
             inputString = inputString.slice(0, inputString.length - 1);
             break;
 
         case "=":
-            let answer = eval(inputString);
-            inputString = answer;
-            input.innerHTML = "(" + input.innerHTML + ")";
-            console.log(answer);
-            output.innerText = answer;
+            try {
+                let answer = eval(inputString);
+                inputString = answer;
+                input.innerText = "(" + input.innerHTML + ")";
+                output.innerText = answer;
+                output.style.color = "black";
+
+            } catch (error) {
+                showError("invalid input");
+            }
             break;
 
         case operatorNumber.includes(clickedButtonId) ? clickedButtonId: false:
-            console.log("adding");
-            if(clickedButtonId === "mod") {
-                inputString += "%";
-            } else if(clickedButtonId === "÷") {
-                inputString += "/";
-            } else {
-                inputString += clickedButtonId;
-            }
+            inputString += validString(clickedButtonId);
+            console.log(inputString);
             input.innerText += clickedButtonId;
-            console.log("user", input.innerText);
-            console.log("computer", inputString);
+        
+        case xTypefunction.includes(clickedButtonId) ? clickedButtonId: false:
 
-         default:
-            console.log("user", input.innerText);
-            console.log("computer", inputString);
+            let ePowOrtenPow = clickedButtonId.substring(clickedButtonId.length - 2, clickedButtonId.length);
+            if(ePowOrtenPow === "**" && clickedButtonId.length > 2) {
+                input.innerText += clickedButtonId.slice(0,-2) + "^";
+                if(clickedButtonId.slice(0,-2) === "e") {
+                    inputString += "2.718282**";
+                } else {
+                    inputString += clickedButtonId;
+                }
+
+            } else if(clickedButtonId === "**" || clickedButtonId === "**2"  || clickedButtonId === "**1/2" || clickedButtonId === "||" || clickedButtonId === "e**") {
+                if(input.innerText.length > 0) {
+                    switch (clickedButtonId) {
+                        case "**":
+                            if(validInput(inputString)) {
+                                input.innerText += "^";
+                                inputString += clickedButtonId;
+                            } else {
+                                showError("invalid input");
+                            }               
+                            break;
+                        case "**2":
+                            if(validInput(inputString)) {
+                                input.innerText += "^2";
+                                inputString += clickedButtonId;
+                            } else {
+                                showError("invalid input");
+                            }
+                            break;
+                        case "**1/2":
+                            charArray = input.innerText.split("");
+                            charArray.unshift("√(");
+                            charArray.push(")");
+                            input.innerText = charArray.join("");
+                            inputString = "Math.sqrt(" + inputString + ")";
+                            break;
+                        case "||":
+                            inputString = "Math.abs(" + inputString + ")";
+                            innput.innerText = "|" + input.innerText + "|";
+                        default:
+                            break;
+                    }   
+                } else {
+                    showError("provide input first");
+                }
+            } else if(clickedButtonId === "**-1") {
+                if(input.innerText.length === 0){
+                    input.innerText += "1/";
+                    inputString += "1/";
+                } else {
+                    showError("invalid input");
+                }
+            } else {}
+            break;
+        default:
             break;
     }
 }
