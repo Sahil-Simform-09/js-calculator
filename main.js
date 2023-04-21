56990f6b7c8b87fec9bc648df9e2cc50a80d500b
Sahil Mistry <sahil.m@simformsolutions.com>
2023-04-21 12:13:37 +0530

    feat: add factorial functionality

--- main.js
+++ main.js
@@ -40,10 +40,9 @@ function validUserString(givenString) {
         .replaceAll("nis", "sin^-1(")
         .replaceAll("soc", "cos^-1(")
         .replaceAll("nat", "tan^-1("));
-
 }  
 
-// get clickedbutton & display, showError
+// DOM manipulation functions
 const getclickedButtonId = function(event) {
     const clickedButton = event.target;
     let isSvgOrPath =  clickedButton instanceof SVGElement || clickedButton instanceof SVGPathElement;  // check clicked button is SVG or path
@@ -69,37 +68,6 @@ const showError = function(errMsg) {
     output.innerText = errMsg;
     output.style.color = "red";
 }
-const validInput = function(inputMessage) {
-    if(Number(inputMessage) !== NaN) {
-        console.log(Number(inputMessage));
-        return true;
-    } 
-    return false;
-}
-
-const memoryOperation = function(clickedButtonId, input, output) {
-    if(clickedButtonId === "MC") {
-        memoryStorage = [];
-    } else if(clickedButtonId === "MR") {
-        if(memoryStorage.length > 0) {
-            inputString += input.innerText += memoryStorage[0];
-        }
-        // inputString += input.innerText += memoryStorage[0] !== undefined ? memoryStorage[0] : 0;
-    } else if(clickedButtonId === "M+" || clickedButtonId === "M-" || clickedButtonId === "MS") {
-        if(output.innerText.length > 0) {
-            if(clickedButtonId === "MS") {
-                
-                memoryStorage[0] = output.innerText;
-            } else {
-                let getValue = memoryStorage[0];
-                memoryStorage[0] = eval(Number(output.innerText) + clickedButtonId.charAt(1) + getValue);
-            }
-        } else {
-            showError("get answer first");
-        }
-    } 
-}
-
 const enableMemoryButton = function(buttons) {
     buttons.forEach(button => {
         button.style.cursor = "pointer";
@@ -114,15 +82,66 @@ const disableMemoryButton = function(buttons) {
     }); 
 }
 
+// calculator logic functions
+const validInput = function(inputMessage) {
+    if(Number(inputMessage) !== NaN) {
+        console.log(Number(inputMessage));
+        return true;
+    } 
+    return false;
+}
+const validFactString = function(factString) {
+    let barackets = 0;
+    let index = 0;
+    for(let i = 0; i < factString.length; i += 1) {
+        if(factString.charAt(i) === "(") {
+            barackets += 1;
+            index = 1;
+        } else if(factString.charAt(i) === ")") {
+            barackets -= 1;
+        }
+    }
+
+    return barackets === 0 ? [true, index] : [false, -1];
+}
+const memoryOperation = function(clickedButtonId, input, output) {
+    if(clickedButtonId === "MC") {
+        memoryStorage = [];
+    } else if(clickedButtonId === "MR") {
+        if(memoryStorage.length > 0) {
+            inputString += input.innerText += memoryStorage[0];
+        }
+    } else if(clickedButtonId === "M+" || clickedButtonId === "M-" || clickedButtonId === "MS") {
+        if(output.innerText.length > 0) {
+            if(clickedButtonId === "MS") {
+                
+                memoryStorage[0] = output.innerText;
+            } else {
+                let getValue = memoryStorage[0];
+                memoryStorage[0] = eval(Number(output.innerText) + clickedButtonId.charAt(1) + getValue);
+            }
+        } else {
+            showError("get answer first");
+        }
+    } 
+}
+const fact = function(number) {
+    let ans = 1;
+    for(let i = 1; i <= number; i += 1) {
+        ans *= i;
+    }
+    return ans;
+}
+
 function handleClick(event) {
 
     let input = getDisplay()[0];
     let output = getDisplay()[1];
 
     let clickedButtonId = getclickedButtonId(event); 
-    
-    //remove error
-    document.querySelector(".error").innerText = "";
+
+    // clear output screen
+    output.innerText = "";
 
     switch (clickedButtonId) {
         case "AC":
@@ -136,7 +155,41 @@ function handleClick(event) {
 
         case "=":
             try {
-                let answer = eval(inputString);              
+                let answer, answerString;
+                const getFactString = [];            
+
+                if(inputString.indexOf("!") !== -1) {
+                    let index = inputString.indexOf("!");
+                    let char = inputString.charAt(index - 1);
+
+                    if(number.includes(char)) {
+                        
+                        let i = index - 1;
+                        while(number.includes(char)) {
+                            i -= 1;
+                            char = inputString.charAt(i);
+                        }
+                        getFactString[0] = inputString.substring(i+1, index);
+                        getFactString[1] = i;
+                        getFactString[2] = index;
+                    } else if(char === ")") {
+
+                        let isValid = validFactString(inputString)[0];
+                        if(isValid[0]) {
+                            getFactString = eval(inputString.substring(isValid[1], index));
+                        } else {
+                            showError("invalid input");
+                        }
+                    }
+
+                    let factorial = fact(getFactString[0]).toString();
+                    answerString = inputString.substring(0, getFactString[1] + 1) + factorial + inputString.substring(getFactString[2] + 1, inputString.length);
+                } else {
+                    answerString = inputString
+                }
+                
+                console.log(answerString);
+                answer = eval(answerString);
 
                 if(answer.toString() == "NaN") {
                     throw new Error("");
@@ -148,7 +201,8 @@ function handleClick(event) {
                 output.style.color = "black";
 
             } catch (error) {
-                showError("invalid input");
+                console.log(error);
+                showError("Error");
             }
             break;
 
@@ -192,6 +246,9 @@ function handleClick(event) {
                     showError("get answer first");
                 }
 
+            } else if(clickedButtonId === "||") {
+                input.innerText = "|" + input.innerText + "|";
+                inputString = "Math.abs(" + inputString + ")";
             } else {
                 input.innerText += validUserString(clickedButtonId);
                 inputString += validComputerString(clickedButtonId);
@@ -200,10 +257,17 @@ function handleClick(event) {
         
         case Trigonometry.includes(clickedButtonId) ? clickedButtonId: false:
             input.innerText += validUserString(clickedButtonId);
-            inputString += validComputerString(clickedButtonId);    
+            inputString += validComputerString(clickedButtonId);  
+            break;  
         
         case memory.includes(clickedButtonId) ? clickedButtonId: false:
             memoryOperation(clickedButtonId, input, output);
+            break;
+
+        case "!":
+            input.innerText += "!";
+            inputString += "!";
+            break;
         default:
             break;
     }
